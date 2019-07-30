e2fd59edda3a66e3f547b323e5b61817605e1009
Joel <joel@gamstudio.net>
2019-07-30 14:22:12 +0800

    加入pb

---
+++ src/server/msg/gen_cp.ps1
@@ -0,0 +1,2 @@
+protoc --go_out=. msg.proto
+cp .\msg.pb.go C:\Users\Joel\Documents\GitHub\ddz_test\src\msg
--- src/server/msg/msg.go
+++ src/server/msg/msg.go
@@ -1,11 +1,9 @@
 package msg
 
-import (
-	"github.com/name5566/leaf/network"
-)
+import "github.com/name5566/leaf/network/protobuf"
 
-var Processor network.Processor
+var Processor = protobuf.NewProcessor()
 
 func init() {
-
+	Processor.Register()
 }
