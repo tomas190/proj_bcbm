f0c80400951203699a7ea911b7c56438d7ef101e
Joel <joel@gamstudio.net>
2019-08-28 15:03:29 +0800

    玩家下注fix

--- src/server/game/internal/handler_room.go
+++ src/server/game/internal/handler_room.go
@@ -22,29 +22,31 @@ func (dl *Dealer) handleBet(args []interface{}) {
 		errorResp(a, msg.ErrorCode_InsufficientBalanceBet, "没钱玩啥")
 	}
 
-	// 够 记录
-	// 在中心服务器减钱，拿返回的余额
 	dl.AreaBets[m.Area] = dl.AreaBets[m.Area] + cs
 	dl.UserBets[au.UserID][m.Area] = dl.UserBets[au.UserID][m.Area] + cs
 
-	//c4c.UserLoseScore(au.UserID, 1, func(data *User) {
-	//	fmt.Println("handleBet 减钱回调")
-	//})
+	c4c.UserLoseScore(au.UserID, -cs, func(data *User) {
+		log.Debug("用户 %+v 下注后余额 %+v", data.UserID, data.Balance)
+		au.Balance = data.Balance
 
-	if dl.Status == constant.RSBetting {
-		resp := &msg.BetInfoB{
-			Area:        m.Area,
-			Chip:        m.Chip,
-			AreaTotal:   dl.AreaBets[m.Area],
-			PlayerTotal: dl.UserBets[au.UserID][m.Area],
-			PlayerID:    au.UserID,
-			Money:       au.Balance - dl.UserBets[au.UserID][m.Area], // fixme
+		fmt.Println("#########区域总和", au.NickName, dl.AreaBets)
+		fmt.Println("@@@@@@@@@玩家各区域投注", au.NickName, dl.UserBets[au.UserID])
+
+		if dl.Status == constant.RSBetting {
+			resp := &msg.BetInfoB{
+				Area:        m.Area,
+				Chip:        m.Chip,
+				AreaTotal:   dl.AreaBets[m.Area],
+				PlayerTotal: dl.UserBets[au.UserID][m.Area],
+				PlayerID:    au.UserID,
+				Money:       au.Balance,
+			}
+
+			dl.Broadcast(resp)
+		} else {
+			errorResp(au.ConnAgent, msg.ErrorCode_NotInBetting, "当前不是下注状态")
 		}
-
-		dl.Broadcast(resp)
-	} else {
-		errorResp(au.ConnAgent, msg.ErrorCode_NotInBetting, "当前不是下注状态")
-	}
+	})
 }
 
 func (dl *Dealer) handlePlayers(args []interface{}) {
@@ -123,9 +125,4 @@ func (dl *Dealer) getPlayerInfoResp() []*msg.UserInfo {
 	playerInfoResp = append(playerInfoResp, &userInfo1, &userInfo2, &userInfo3, &userInfo4, &userInfo5, &userInfo1, &userInfo2, &userInfo3, &userInfo4, &userInfo5)
 
 	return playerInfoResp
-}
-
-// 庄家列表
-func (dl *Dealer) getBankerInfoResp() []*msg.UserInfo {
-	return nil
 }
