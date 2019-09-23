58ac4c20010b34bab6ae4c1934f751afe03a3721
Joel <joel@gamstudio.net>
2019-09-23 13:51:03 +0800

    延迟调整

--- src/server/game/internal/handler_room.go
+++ src/server/game/internal/handler_room.go
@@ -125,7 +125,7 @@ func (dl *Dealer) handleAutoBet(args []interface{}) {
 		})
 
 		// 暂时延迟处理
-		time.Sleep(time.Millisecond * 1)
+		time.Sleep(time.Millisecond * 2)
 	}
 	dl.UserAutoBet[au.UserID] = true
 }
