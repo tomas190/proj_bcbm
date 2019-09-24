9558f25f3e33d4be0d2afca7e2cfe8ae5dfb99db
Joel <joel@gamstudio.net>
2019-09-24 09:22:01 +0800

    fix lock

--- src/server/game/internal/c4c_test.go
+++ src/server/game/internal/c4c_test.go
@@ -1,19 +1,19 @@
 package internal
 
 import (
-	"fmt"
-	"github.com/name5566/leaf/log"
 	"testing"
 	"time"
+
+	"github.com/name5566/leaf/log"
 )
 
 func TestClient4Center_ServerLoginCenter(t *testing.T) {
 	c := NewClient4Center()
-	c.ReqToken()
+	// c.ReqToken()
 	c.HeartBeatAndListen()
 	time.Sleep(5 * time.Second)
 
-	c.CronUpdateToken()
+	// c.CronUpdateToken()
 
 	for {
 		// 在没有收到服务器登陆成功返回之前不应该执行后续操作
@@ -23,35 +23,25 @@ func TestClient4Center_ServerLoginCenter(t *testing.T) {
 			log.Debug("<----用户登录回调---->%+v %+v %+v", data.Balance, data.NickName, data.Avatar)
 		})
 
-		fmt.Println("#####", c.userWaitEvent)
-
 		time.Sleep(7 * time.Second)
 
-		c.UserLoseScore(userID, -5, "",
+		c.UserLoseScore(userID, -5, "", "",
 			func(data *User) {
 				log.Debug("<----用户减钱回调---->%+v %+v %+v", data.Balance, data.NickName, data.Avatar)
 			})
 		time.Sleep(7 * time.Second)
 
-		fmt.Println(c.userWaitEvent)
-
 		c.UserLogoutCenter(userID, func(data *User) {
 			log.Debug("<----用户登出回调---->%+v", data.UserID)
 		})
 
-		fmt.Println("#####", c.userWaitEvent)
-
 		time.Sleep(7 * time.Second)
-
-		fmt.Println("#####", c.userWaitEvent)
 
 		c.UserLoginCenter(userID, "123456", func(data *User) {
 			log.Debug("<----用户登录回调---->%+v %+v %+v", data.Balance, data.NickName, data.Avatar)
 		})
 
 		time.Sleep(7 * time.Second)
-
-		fmt.Println("#####", c.userWaitEvent)
 	}
 }
 
@@ -60,7 +50,7 @@ func TestClient4Center_MinusMoney(t *testing.T) {
 	userID := uint32(516499995)
 
 	c := NewClient4Center()
-	c.ReqToken()
+	// c.ReqToken()
 	c.HeartBeatAndListen()
 	time.Sleep(1 * time.Second)
 
@@ -70,13 +60,11 @@ func TestClient4Center_MinusMoney(t *testing.T) {
 
 	time.Sleep(2 * time.Second)
 
-	c.UserLoseScore(userID, -1000, "",
+	c.UserLoseScore(userID, -1000, "", "",
 		func(data *User) {
 			log.Debug("<----用户减钱回调---->%+v %+v", data.UserID, data.Balance)
 		})
 	time.Sleep(2 * time.Second)
-
-	fmt.Println(c.userWaitEvent)
 }
 
 // 加钱
@@ -87,7 +75,7 @@ func TestClient4Center_AddMoney(t *testing.T) {
 	userID := uint32(547874059)
 
 	c := NewClient4Center()
-	c.ReqToken()
+	// c.ReqToken()
 	c.HeartBeatAndListen()
 	time.Sleep(1 * time.Second)
 
@@ -97,13 +85,11 @@ func TestClient4Center_AddMoney(t *testing.T) {
 
 	time.Sleep(2 * time.Second)
 
-	c.UserWinScore(userID, 20000, "test-order-add",
+	c.UserWinScore(userID, 20000, "test-order-add", "",
 		func(data *User) {
 			log.Debug("<----用户加钱回调---->%+v %+v", data.UserID, data.Balance)
 		})
 	time.Sleep(5 * time.Second)
-
-	fmt.Println(c.userWaitEvent)
 }
 
 // 测试中心服重连和断连
