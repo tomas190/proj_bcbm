fc45c80ea9e00d13c9b23e255f5ebb8017011c92
tomas <tomas@gamstudio.net>
2019-12-23 18:06:46 +0800

    处理限额投注

--- src/server/gate/internal/module.go
+++ src/server/gate/internal/module.go
@@ -15,7 +15,7 @@ func (m *Module) OnInit() {
 	m.Gate = &gate.Gate{
 		MaxConnNum:      conf.Server.MaxConnNum,
 		PendingWriteNum: conf.PendingWriteNum,
-		MaxMsgLen:       conf.MaxMsgLen,
+		MaxMsgLen:       2 * 1024 * 1024, //conf.MaxMsgLen  todo
 		WSAddr:          conf.Server.WSAddr,
 		HTTPTimeout:     conf.HTTPTimeout,
 		CertFile:        conf.Server.CertFile,
