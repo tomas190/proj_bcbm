4eb6b9fb667a87a97298da08e4077d7a8c4a88d5
tomas <tomas@gamstudio.net>
2020-08-27 15:34:10 +0800

    机器人投注机策略调整

--- src/server/game/internal/bot.go
+++ src/server/game/internal/bot.go
@@ -88,45 +88,34 @@ func (dl *Dealer) AddBots() {
 		handleNum = 80
 		break
 	}
-	var num2 int
-	if len(dl.Bots) > 20 {
-		for k, v := range dl.Bots {
-			if v != nil {
-				dl.Bots = append(dl.Bots[:k], dl.Bots[k+1:]...)
-				num2 ++
-				if num2 >= 20 {
-					break
-				}
-			}
-		}
-	}else if len(dl.Bots) < 15 {
-		for k, v := range dl.Bots {
-			if v != nil {
-				dl.Bots = append(dl.Bots[:k], dl.Bots[k+1:]...)
-				num2 ++
-				if num2 >= 10 {
-					break
-				}
-			}
-		}
-	}
 
-	//var randNum int
+	var randNum int
 	slice := []int32{1, 2, 1, 2} // 1为-,2为+
 	rand.Seed(time.Now().UnixNano())
 	num := rand.Intn(len(slice))
+	var maNum float64
 	if slice[num] == 1 {
 		getNum := handleNum / 10
-		maNum := math.Floor(float64(getNum))
+		maNum = math.Floor(float64(getNum))
 		handleNum -= int(maNum)
-		//randNum = int(maNum)
+		randNum = int(maNum)
 	} else if slice[num] == 2 {
 		getNum := handleNum / 10
-		maNum := math.Floor(float64(getNum))
-		//RNum := float64(handleNum) * 0.25
-		//RNNum := math.Floor(RNum)
+		maNum = math.Floor(float64(getNum))
+		RNum := float64(handleNum) * 0.25
+		RNNum := math.Floor(RNum)
 		handleNum += int(maNum)
-		//randNum = int(RNNum)
+		randNum = int(RNNum)
+	}
+	var num3 int
+	for k, v := range dl.Bots {
+		if v != nil {
+			dl.Bots = append(dl.Bots[:k], dl.Bots[k+1:]...)
+			robotNum = len(dl.Bots)
+			if num3 >= int(maNum) {
+				return
+			}
+		}
 	}
 
 	if robotNum < handleNum { // 加
@@ -150,18 +139,20 @@ func (dl *Dealer) AddBots() {
 			}
 		}
 	}
-	//var num2 int
-	//for _, v := range dl.Bots {
-	//	if v != nil {
-	//		r := util.Random{}
-	//		v.UserID = uint32(100000000 + r.RandInRange(0, 200000000))
-	//		v.Balance = float64(0+r.RandInRange(200, 4600)) + float64(r.RandInRange(50, 100))/100.0 // 金币数
-	//		num2++
-	//		if num2 >= randNum {
-	//			return
-	//		}
-	//	}
-	//}
+
+	var num2 int
+	for _, v := range dl.Bots {
+		if v != nil {
+			r := util.Random{}
+			v.UserID = uint32(100000000 + r.RandInRange(0, 200000000))
+			v.Balance = float64(0+r.RandInRange(200, 4600)) + float64(r.RandInRange(50, 100))/100.0 // 金币数
+			v.BetAmount = float64(r.RandInRange(20, 500))
+			num2++
+			if num2 >= randNum {
+				return
+			}
+		}
+	}
 
 	betGod := dl.BetGod()
 	nextBankerBot := dl.NextBotBanker()
